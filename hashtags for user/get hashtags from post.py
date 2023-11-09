2c8f39bef8b5c0fd01aee3e52b91a95d021dfb6c
ERR0RW0LF <98032358+ERR0RW0LF@users.noreply.github.com>
2023-11-09 20:04:52 +0100

    Update get hashtags from post.py

--- hashtags for user/get hashtags from post.py
+++ hashtags for user/get hashtags from post.py
@@ -1,1 +1,40 @@
+import pandas
+import os
 
+
+user = "113231132311323113231132311323" #User ID
+if os.path(f"hashtags {user}.csv"):
+    pass
+else:
+    f = open(f"hashtags {user}.csv", "w")
+    f.write("hashtag,post with hashtag viewed,time,liked,disliked,comments,own post with hashtag,overall score")
+df = pandas.read_csv(f"hashtags {user}.csv", index_col="hashtag")
+
+hashtags1 = [] #definiert die Liste hashtag1
+check = "#" #da durch wir jeder hastag erkannt
+splitPostSubSentinses = []
+post = "Dies ist ein #Test Post für #Python. So das ist, der zweite, Satz ich nutze #VS-Code um zu #programmieren." #Post von dem man den Hastag haben will
+print(post) #druckt Post in Terminal nur zum debuggen
+
+postdot = post.rfind(".") #Findet vom hintersten Punkt im Post die Position
+post = post[:postdot] #Löscht den hintersten Punkt im Post
+print(post) #druckt Post in Terminal nur zum debuggen
+
+splitPostSentinses = post.split(". ") #trent den Post an jebem Punkt mit Lehrzeichen danach und schreibt in Liste ohne Punkte
+print(splitPostSentinses) #druckt List von forheriger Line in Terminal nur zum debuggen
+
+for satz in splitPostSentinses:
+    if ', ' in satz:
+        splitPostSubSentinses.extend(satz.split(", "))
+    else:
+        splitPostSubSentinses.append(satz)
+
+print(splitPostSubSentinses)
+
+for subSatz in splitPostSubSentinses:                                                       #(
+    hashtags = [idx for idx in subSatz.split() if idx[0].lower() == check.lower()]          # sucht alle hashtags aus den Sätzen und fügt sie alle als einzelne Items in die Liste hashtags1
+    hashtags1.extend(hashtags)                                                              #)
+
+print(hashtags1) #druckt List hashtags1 in Terminal nur zum debuggen
+
+print(df)
