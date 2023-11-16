9d4771cee9c8eab4d5ce699fa23720ca34dd4efd
ERR0RW0LF <98032358+ERR0RW0LF@users.noreply.github.com>
2023-11-16 08:47:18 +0100

    updated code

--- hashtags for user/hashtags_from_post.py
+++ hashtags for user/hashtags_from_post.py
@@ -2,10 +2,18 @@ import pandas as pd
 import os.path
 import random
 from pandas import *
+import sys
+
+n = len(sys.argv)
 
 
-user = "113231132311323113231132311323" #User ID TODO: must be changed when I add the apis to the code 
-userFile = ".\hashtags " + user + ".csv"
+
+if len(sys.argv[1]) == 30:
+    user = str(sys.argv[1]) #User ID TODO: neat to add a check if the id is valid 
+else:
+    exit()
+
+userFile =  "hashtags-" + user + "-.csv"
 if os.path.isfile(userFile):
     pass
 else:
@@ -18,27 +26,32 @@ df = pd.read_csv(userFile, index_col=0, sep=",")
 
 hashtags1 = [] #definiert die Liste hashtag1
 hashtags2 = []
-check = "#" #da durch wir jeder hastag erkannt
-splitPostSubSentinses = []
-post = "Dies ist ein #Test Post für #Python. So das ist, der zweite, Satz ich nutze #VS-Code um zu #programmieren." #Post von dem man den Hastag haben will TODO: must be changed when I add the apis to the code 
+check = "#" #da durch wir jeder hashtag erkannt
+splitPostSubSentences = []
+post = "Dies ist ein #Test Post für #Python. So das ist, der zweite, Satz ich nutze #VS-Code um zu #programmieren." #Post von dem man den Hashtag haben will TODO: must be changed when I add the apis to the code 
 print(post) #druckt Post in Terminal nur zum debuggen
 
-postdot = post.rfind(".") #Findet vom hintersten Punkt im Post die Position
-post = post[:postdot] #Löscht den hintersten Punkt im Post
+like = False
+dislike = False
+time = 10
+comment = False
+
+postDot = post.rfind(".") #Findet vom hintersten Punkt im Post die Position
+post = post[:postDot] #Löscht den hintersten Punkt im Post
 print(post) #druckt Post in Terminal nur zum debuggen
 
-splitPostSentinses = post.split(". ") #trent den Post an jebem Punkt mit Lehrzeichen danach und schreibt in Liste ohne Punkte
-print(splitPostSentinses) #druckt List von forheriger Line in Terminal nur zum debuggen
+splitPostSentences = post.split(". ") #trennt den Post an jedem Punkt mit Leerzeichen danach und schreibt in Liste ohne Punkte
+print(splitPostSentences) #druckt List von vorheriger Line in Terminal nur zum debuggen
 
-for satz in splitPostSentinses:
+for satz in splitPostSentences:
     if ', ' in satz:
-        splitPostSubSentinses.extend(satz.split(", "))
+        splitPostSubSentences.extend(satz.split(", "))
     else:
-        splitPostSubSentinses.append(satz)
+        splitPostSubSentences.append(satz)
 
-print(splitPostSubSentinses)
+print(splitPostSubSentences)
 
-for subSatz in splitPostSubSentinses:                                                       # TODO: for ö, ü, ä implement recognition and then replace with oe, ue, ae
+for subSatz in splitPostSubSentences:                                                       # TODO: for ö, ü, ä implement recognition and then replace with oe, ue, ae
     hashtags = [idx for idx in subSatz.split() if idx[0].lower() == check.lower()]          # sucht alle hashtags aus den Sätzen und fügt sie alle als einzelne Items in die Liste hashtags1
     hashtags1.extend(hashtags)                                                              #)
 
@@ -65,17 +78,16 @@ for hashtag in hashtags2:
             df.at[hashtag,"liked"] = df.at[hashtag,"liked"] + 1
         else:
             df.at[hashtag,"disliked"] = df.at[hashtag,"disliked"] + 1
-        
+
         if random.randrange(0,4) == 3:
             df.at[hashtag,"comments"] = df.at[hashtag,"comments"] + 1
         elif random.randrange(0,3) == 2:
             df.at[hashtag,"posted"] = df.at[hashtag,"posted"] + 1
-        
+            
         df.at[hashtag,"score"] = df.at[hashtag,"time"] / df.at[hashtag,"viewed"] + 10 * (df.at[hashtag,"liked"] - df.at[hashtag,"disliked"]) + 5 * df.at[hashtag,"comments"] + 10 * df.at[hashtag,"posted"]
-        #TODO: Add sorting for the hashtags based on ther scores
     else:
         print(hashtag, " is not in csv")
-        
+
         new_row = {hashtag:[1,0,0,0,0,0,0/1+10*(1-0)+5*1+10*0]} #TODO: must be changed when I add the apis to the code 
         df_new_row = pd.DataFrame.from_dict(data=new_row,
                                             orient='index',
