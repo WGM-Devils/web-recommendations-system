a6fc834e57573b92ce806b68d43b036ca61eddaf
ERR0RW0LF <finn.woma@gmail.com>
2023-12-20 17:17:03 +0100

    Add code to retrieve and process posts with IDs

--- hashtags_for_user/request_test.py
+++ hashtags_for_user/request_test.py
@@ -1,5 +1,25 @@
 import requests
+import pandas as pd
 
 uri = "https://hallo.klingt-gut.repl.co/api/posts/all"
 
-print(requests.get(uri, headers={"Authorization": "KlingtGut"}).json())
+posts = requests.get(uri, headers={"Authorization": "KlingtGut"}).json()
+
+print(posts)
+
+n = 0
+
+posts_with_ids = []
+
+for post in posts["posts"]:
+    print(post)
+    n += 1
+    
+    post_id = post["id"]
+    print(post_id)
+    
+    full_post = {post_id: post}
+    
+    posts_with_ids.append(full_post)
+
+print(posts_with_ids)
