ac78ea0538e040440393a3d1e288bf44e70a94f4
ERR0RW0LF <finn.woma@gmail.com>
2023-12-20 15:17:07 +0100

    Add import statement for requests module

--- hashtags_for_user/hashtags_from_post.py
+++ hashtags_for_user/hashtags_from_post.py
@@ -7,6 +7,7 @@ import os.path
 import random
 from pandas import *
 import sys
+import requests
 
 n = len(sys.argv)
 
---
+++ hashtags_for_user/request_test.py
@@ -0,0 +1,5 @@
+import requests
+
+uri = "https://hallo.klingt-gut.repl.co/api/posts/all"
+
+print(requests.get(uri, headers={"Authorization": "KlingtGut"}))
